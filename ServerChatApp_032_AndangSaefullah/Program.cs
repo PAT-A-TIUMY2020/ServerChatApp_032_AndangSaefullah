edb674fed1b847651cb55e910d81f0c08f4bd0c2
Your Name <andangsaefullah4@gmail.com>
2021-01-12 13:41:23 +0700

    Add project files.

---
+++ ServerChatApp_032_AndangSaefullah/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_032;
+
+namespace ServerChatApp_032_AndangSaefullah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ServiceHost hostObject = null;
+
+            try
+            {
+                hostObject = new ServiceHost(typeof(ServiceCallback));
+                hostObject.Open();
+
+                Console.WriteLine("Serves On, Ready to use");
+                Console.ReadLine();
+
+                hostObject.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObject = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
